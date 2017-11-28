7431304b968df3ddeb714abbb8f3a61cb53e5d20
Hrabal <federicocerchiari@gmail.com>
2017-11-28 22:29:46 +0100

    ajax wonderful engine

--- static/js/main.js
+++ static/js/main.js
@@ -3,8 +3,12 @@ $(document).ready(function() {
 		$(this).find(".menuItem").slideToggle("slow");
 	});
 	$('.menuItem').click(function() {
+		var link = $(this).attr("link");
 		$('#title').animate({
 			marginTop: "0vh"
 		}, 1000);
+		$.get(link, function(data) {
+			$("#content").html(data)
+		});
 	});
 });
